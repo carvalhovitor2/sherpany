531c65b47864b7a2d6d096a4ef0b07786d93dd91
Vitor Carvalho <vitoracarvalho@gmail.com>
2023-04-12 20:44:55 +0100

    Actually doing validations instead of just printing hello world

--- .github/workflows/scripts/validate.sh
+++ .github/workflows/scripts/validate.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo "Hello world" && pwd
+cd terraform
+
+terraform validate
+
+terraform fmt -check=true -diff=true -recursive=true
+
+terraform validate -var-file=my-variables.tfvars
