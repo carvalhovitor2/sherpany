e411b32b4ea3575bcacd7eafbb8fe1578c61277c
Vitor Carvalho <vitoracarvalho@gmail.com>
2023-04-13 16:33:42 +0100

    Adding helm and EBS CSi

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -55,3 +55,20 @@ resource "kubernetes_namespace" "sherpany" {
     }
   }
 }
+
+
+module "ebs_csi_driver_controller" {
+  source = "DrFaust92/ebs-csi-driver/kubernetes"
+
+  ebs_csi_controller_image                   = ""
+  ebs_csi_controller_role_name               = "ebs-csi-driver-controller"
+  ebs_csi_controller_role_policy_name_prefix = "ebs-csi-driver-policy"
+  oidc_url                                   = aws_iam_openid_connect_provider.openid_connect.url
+}
+
+resource "aws_iam_openid_connect_provider" "openid_connect" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.cert.certificates.0.sha1_fingerprint]
+  url             = data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer
+}
+
