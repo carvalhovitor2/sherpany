8b242940c9f7e7cf46ebf9bfb8f2c8fef8fa2fcb
Vitor Carvalho <vitoracarvalho@gmail.com>
2023-04-15 23:04:18 +0100

    I also need the kubectl provider

--- terraform/init.tf
+++ terraform/init.tf
@@ -6,9 +6,14 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = "false"
 }
 
+provider "kubectl" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+}
 provider "helm" {
   kubernetes {
     host                   = data.aws_eks_cluster.cluster.endpoint
@@ -23,5 +28,12 @@ terraform {
     encrypt        = "true"
     key            = "terraform.tfstate"
     region         = "eu-central-1"
+  }
+
+  required_providers {
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
+    }
   }
 }
